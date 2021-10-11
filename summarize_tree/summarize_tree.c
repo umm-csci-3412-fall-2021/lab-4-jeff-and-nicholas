29489540cca9e8c7085c7f730bec157b7685ad17
gilb0578 <ninjamipsmurf@gmail.com>
2021-10-10 21:49:56 -0500

    Implemented process_directory, is_dir, and process_file.
    Will run when full path is given, struggling with partial path.

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -9,13 +9,13 @@
 static int num_dirs, num_regular;
 
 bool is_dir(const char* path) {
-  /*
-   * Use the stat() function (try "man 2 stat") to determine if the file
-   * referenced by path is a directory or not.  Call stat, and then use
-   * S_ISDIR to see if the file is a directory. Make sure you check the
-   * return value from stat() in case there is a problem, e.g., maybe the
-   * the file doesn't actually exist.
-   */
+  struct stat   buf;
+  if (stat(path, &buf)==0){
+    return S_ISDIR(buf.st_mode);
+  }
+  else{
+    return false;
+  }
 }
 
 /* 
@@ -25,17 +25,19 @@ bool is_dir(const char* path) {
 void process_path(const char*);
 
 void process_directory(const char* path) {
-  /*
-   * Update the number of directories seen, use opendir() to open the
-   * directory, and then use readdir() to loop through the entries
-   * and process them. You have to be careful not to process the
-   * "." and ".." directory entries, or you'll end up spinning in
-   * (infinite) loops. Also make sure you closedir() when you're done.
-   *
-   * You'll also want to use chdir() to move into this new directory,
-   * with a matching call to chdir() to move back out of it when you're
-   * done.
-   */
+  chdir(path);
+  struct dirent *entry;
+  DIR *dir;
+  dir = opendir(path);
+  while((entry = readdir(dir)) != NULL){
+    if(strcmp(".",entry->d_name) != 0 && strcmp("..",entry->d_name) != 0){
+      char pathTo[1024];
+      snprintf(pathTo, sizeof(pathTo), "%s/%s", path, entry->d_name);
+      process_path(pathTo);
+    }
+  }
+  closedir(dir);
+  chdir("..");
 }
 
 void process_file(const char* path) {
@@ -43,10 +45,12 @@ void process_file(const char* path) {
    * Update the number of regular files.
    * This is as simple as it seems. :-)
    */
+  num_regular++;
 }
 
 void process_path(const char* path) {
   if (is_dir(path)) {
+    num_dirs++;
     process_directory(path);
   } else {
     process_file(path);
